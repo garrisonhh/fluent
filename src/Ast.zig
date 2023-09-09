761fea1351dc0e6c269fa4254dc44fc3bad4457e
garrisonhh <garrisonhh@pm.me>
2023-09-09 13:41:20 -0400

    scaffolding ast repr

--- src/Ast.zig
+++ src/Ast.zig
@@ -4,20 +4,60 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const com = @import("common");
 
-pub const Node = union(enum) {
+pub const Node = com.Ref(.ast_node, 32);
+const NodeMap = com.RefMap(Node, Expr);
+
+pub const UnaryOp = enum {
+    negate,
+};
+
+pub const BinaryOp = enum {
+    add,
+    subtract,
+    multiply,
+    divide,
+};
+
+pub const Expr = union(enum) {
     const Self = @This();
     pub const Tag = std.meta.Tag(Self);
 
-    pub const Ref = com.Ref(.ast_node, 32);
-    const Map = com.RefMap(Ref, Self);
+    pub const Unary = struct {
+        op: UnaryOp,
+        child: Node,
+    };
 
+    pub const Binary = struct {
+        op: BinaryOp,
+        lhs: Node,
+        rhs: Node,
+    };
 
+    ident: []const u8,
+    parens: Node,
+    unary: Unary,
+    binary: Binary,
+
+    fn deinit(self: Self, ally: Allocator) void {
+        switch (self) {
+            .parens, .unary, .binary => {},
+            .ident => |str| ally.free(str),
+        }
+    }
 };
 
 const Ast = @This();
 
-map: Node.Map = .{},
+map: NodeMap = .{},
 
 pub fn deinit(self: *Ast, ally: Allocator) void {
     self.map.deinit(ally);
+}
+
+pub fn new(self: *Ast, ally: Allocator, expr: Expr) Allocator.Error!Node {
+    return try self.map.put(ally, expr);
+}
+
+pub fn get(self: *const Ast, node: Node) *const Expr {
+    return self.map.get(node);
 }
