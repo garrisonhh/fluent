fbf0c3bb54f131d8a9932f2e49a7611b9676ea1f
garrisonhh <garrisonhh@pm.me>
2023-09-11 15:40:46 -0400

    more tests

--- src/tests.zig
+++ src/tests.zig
@@ -9,6 +9,8 @@ const TestFailure = error.TestFailure;
 
 const ally = std.testing.allocator;
 
+// helpers =====================================================================
+
 fn expectExpr(ast: *Ast, expected: Ast.Node, text: []const u8) !void {
     const node = try parser.parseFragment(ally, ast, text, .expr) orelse {
         return TestFailure;
@@ -42,32 +44,59 @@ fn expectExpr(ast: *Ast, expected: Ast.Node, text: []const u8) !void {
     return TestFailure;
 }
 
-fn testIdent(ast: *Ast, ident: []const u8) !void {
+fn testIdent(ident: []const u8) !void {
+    var ast = Ast{};
+    defer ast.deinit(ally);
+
     const expr = try ast.new(ally, .{ .ident = try ally.dupe(u8, ident) });
-    try expectExpr(ast, expr, ident);
+    try expectExpr(&ast, expr, ident);
 }
 
-fn testInt(ast: *Ast, int: Ast.Expr.Int) !void {
+fn testInt(int: Ast.Expr.Int) !void {
+    var ast = Ast{};
+    defer ast.deinit(ally);
+
     const expr = try ast.new(ally, .{ .int = int });
 
     const text = try std.fmt.allocPrint(ally, "{d}", .{int});
     defer ally.free(text);
 
-    try expectExpr(ast, expr, text);
+    try expectExpr(&ast, expr, text);
 }
 
-test "parsing-basic" {
+fn testReal(real: Ast.Expr.Real) !void {
     var ast = Ast{};
     defer ast.deinit(ally);
 
-    const unit = try ast.new(ally, .unit);
-    try expectExpr(&ast, unit, "()");
+    const expr = try ast.new(ally, .{ .real = real });
 
-    try testIdent(&ast, "hello");
-    try testIdent(&ast, "CamelCase");
-    try testIdent(&ast, "kebab-case");
+    const text = try std.fmt.allocPrint(ally, "{d:.16}", .{real});
+    defer ally.free(text);
 
-    try testInt(&ast, 0);
-    try testInt(&ast, 420);
-    try testInt(&ast, std.math.maxInt(Ast.Expr.Int));
+    try expectExpr(&ast, expr, text);
+}
+
+// tests =======================================================================
+
+test "parse-unit" {
+    var ast = Ast{};
+    defer ast.deinit(ally);
+}
+
+test "parse-identifiers" {
+    try testIdent("hello");
+    try testIdent("CamelCase");
+    try testIdent("kebab-case");
+}
+
+test "parse-ints" {
+    try testInt(0);
+    try testInt(420);
+    try testInt(std.math.maxInt(Ast.Expr.Int));
+}
+
+test "parse-reals" {
+    try testReal(0.0);
+    try testReal(0.0000001);
+    try testReal(1231242.012232132);
 }
