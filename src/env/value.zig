f78f8dab550e67348d9a3ec21acc1154b18e0f08
garrisonhh <garrisonhh@pm.me>
2023-10-11 11:46:43 -0400

    value scaffold

---
+++ src/env/value.zig
@@ -0,0 +1,76 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const com = @import("common");
+const fluent = @import("../mod.zig");
+const Type = fluent.Type;
+const typer = fluent.typer;
+const Ident = @import("idents.zig").Ident;
+const Name = @import("names.zig").Name;
+
+/// raw values used to represent fluent data across the compiler
+pub const Value = union(enum) {
+    const Self = @This();
+    pub const Ref = com.Ref(.value, 64);
+    pub const RefList = com.RefList(Ref, Self);
+
+    pub const UInt = union(enum) {
+        u8: u8,
+        u16: u16,
+        u32: u32,
+        u64: u64,
+    };
+
+    pub const Int = union(enum) {
+        i8: i8,
+        i16: i16,
+        i32: i32,
+        i64: i64,
+    };
+
+    pub const Float = union(enum) {
+        f32: f32,
+        f64: f64,
+    };
+
+    pub const Function = struct {
+        const ParamMap = std.AutoArrayHashMapUnmanaged(Ident, Type.Id);
+
+        type: Type.Id,
+        params: ParamMap = .{},
+        /// null if this function hasn't yet been compiled
+        ssa: ?fluent.ssa.Func.Ref,
+    };
+
+    unit,
+    name: Name,
+    type: Type.Id,
+    bool: bool,
+    uint: UInt,
+    int: Int,
+    float: Float,
+    function: Function,
+
+    pub fn deinit(self: Self, ally: Allocator) void {
+        _ = ally;
+        switch (self) {
+            else => {},
+        }
+    }
+
+    /// determines the type based on the data format (this might be slow)
+    pub fn findType(self: Self) Type.Id {
+        return switch (self) {
+            inline .unit, .name, .type, .bool => |_, tag| predef: {
+                const p = std.enums.nameCast(typer.PredefinedType, tag);
+                break :predef typer.predef(p);
+            },
+            inline .uint, .int, .float => |num| switch (num) {
+                inline else => |_, tag| predef: {
+                    const p = std.enums.nameCast(typer.PredefinedType, tag);
+                    break :predef typer.predef(p);
+                },
+            },
+            .function => |f| f.type,
+        };
+    }
+};
